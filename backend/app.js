852347ae4cce1caa541aed7d9dfc22c26bffd5a4
Satyam Kumar <satyamkumar629301@gmail.com>
2026-01-16 12:40:26 +0530

    setup

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+const app = express();
+
+
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static("uploads"));
+
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+
+const start = () => {
+    mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('Connected!'));
+};
+
+app.listen(8080, () => {
+    console.log("Server running on 8080");
+    start();
+});
+
