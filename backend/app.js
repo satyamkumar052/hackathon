941d4c5edd3deb106c46adc742ee5fb60d7f1555
Satyam Kumar <satyamkumar629301@gmail.com>
2026-01-16 17:27:32 +0530

    complete signup page

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 
-
+import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
@@ -14,6 +14,7 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("uploads"));
+app.use(express.static("public"));
 
 
 app.use("/", UserRouter);
--- backend/controller/user.contoller.js
+++ backend/controller/user.contoller.js
@@ -6,16 +6,15 @@ import crypto from "crypto";
 export const register = async (req, res) => {
     try {
 
-        const { name, email, password, username } = req.body;
+        const {email, password, username } = req.body;
 
-        if (!name || !email || !password || !username) return res.status(400).json({ message: "All fields are required" });
+        if (!email || !password || !username) return res.status(400).json({ message: "All fields are required" });
         
         const user = await User.findOne({ email });
         if (user) return res.status(400).json({message: "User already exists"});
 
         const hashedPassword = await bcrypt.hash (password, 10);
         const newUser = new User({
-            name,
             email,
             password: hashedPassword,
             username
